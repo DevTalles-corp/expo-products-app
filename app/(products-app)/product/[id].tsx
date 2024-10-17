4fc4956b79dd9875483fc3de84089af8831a23a4
Fernando Herrera <fernando.herrera85@gmail.com>
2024-10-17 11:18:46 -0400

    Refactor code to update product screen with React Query and secure storage

--- app/(products-app)/product/[id].tsx
+++ app/(products-app)/product/[id].tsx
@@ -1,9 +1,60 @@
-import { View, Text } from 'react-native';
+import { useEffect } from 'react';
+import {
+  View,
+  Text,
+  KeyboardAvoidingView,
+  ScrollView,
+  Platform,
+} from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { useNavigation } from 'expo-router';
+import { ThemedView } from '@/presentation/theme/components/ThemedView';
+import ThemedTextInput from '@/presentation/theme/components/ThemedTextInput';
+
 const ProductScreen = () => {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    // TODO: colocar el nombre del producto
+
+    navigation.setOptions({
+      headerRight: () => <Ionicons name="camera-outline" size={25} />,
+    });
+  }, []);
+
   return (
-    <View>
-      <Text>ProductScreen</Text>
-    </View>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+    >
+      <ScrollView>
+        {/* TODO: Product Images */}
+
+        <ThemedView style={{ marginHorizontal: 10, marginTop: 20 }}>
+          <ThemedTextInput placeholder="Título" style={{ marginVertical: 5 }} />
+
+          <ThemedTextInput placeholder="Slug" style={{ marginVertical: 5 }} />
+
+          <ThemedTextInput
+            placeholder="Descripción"
+            multiline
+            numberOfLines={5}
+            style={{ marginVertical: 5 }}
+          />
+        </ThemedView>
+
+        <ThemedView
+          style={{
+            marginHorizontal: 10,
+            marginVertical: 5,
+            flexDirection: 'row',
+            gap: 10,
+          }}
+        >
+          <ThemedTextInput placeholder="Precio" style={{ flex: 1 }} />
+          <ThemedTextInput placeholder="Inventario" style={{ flex: 1 }} />
+        </ThemedView>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 export default ProductScreen;
--- presentation/theme/components/ThemedTextInput.tsx
+++ presentation/theme/components/ThemedTextInput.tsx
@@ -13,7 +13,7 @@ interface Props extends TextInputProps {
   icon?: keyof typeof Ionicons.glyphMap;
 }
 
-const ThemedTextInput = ({ icon, ...rest }: Props) => {
+const ThemedTextInput = ({ icon, style, ...rest }: Props) => {
   const primaryColor = useThemeColor({}, 'primary');
   const textColor = useThemeColor({}, 'text');
 
@@ -22,10 +22,13 @@ const ThemedTextInput = ({ icon, ...rest }: Props) => {
 
   return (
     <View
-      style={{
-        ...styles.border,
-        borderColor: isActive ? primaryColor : '#ccc',
-      }}
+      style={[
+        {
+          ...styles.border,
+          borderColor: isActive ? primaryColor : '#ccc',
+        },
+        style,
+      ]}
       onTouchStart={() => inputRef.current?.focus()}
     >
       {icon && (
@@ -58,7 +61,7 @@ const styles = StyleSheet.create({
   border: {
     borderWidth: 1,
     borderRadius: 5,
-    padding: 5,
+    padding: 10,
     marginBottom: 10,
     flexDirection: 'row',
     alignItems: 'center',
